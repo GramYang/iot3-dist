18c3818789f1c94a0d871d1cfccc2e61ad6fc01b
gramyang <18872253531@163.com>
2020-12-28 14:15:38 +0800

    2020 12 28 dist文件分解压缩到了极限

---
+++ dist/js/chunk-2d224b2b.092baee0.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d224b2b"],{e0f0:function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("p",[e._v("首页")])},c=[],u={name:"Home"},o=u,r=t("2877"),s=Object(r["a"])(o,a,c,!1,null,"1f206a3a",null);n["default"]=s.exports}}]);
--- start.go
+++ start.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"github.com/gin-contrib/gzip"
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
-func main(){
-	r:=gin.Default()
+func main() {
+	r := gin.Default()
 	r.Use(static.Serve("/", static.LocalFile("dist", false)))
+	r.Use(gzip.Gzip(gzip.DefaultCompression))
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(200, "test")
 	})
-	_=r.Run(":8086")
+	_ = r.Run(":8085")
 }
